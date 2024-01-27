6d26dc231f546aef49f41e859b1f0ddab3c6968f
Chaitanyalonarkar <chaitanyalonarkar@gmail.com>
2024-01-27 22:00:16 +0530

    added build files

--- src/App.js
+++ src/App.js
@@ -30,13 +30,13 @@ function App() {
       <form className="bmi">
         <h3>BMI Calculator</h3>
         
-        <input type="number" id="weight" placeholder="Enter weight" onChange={(e) => setWeight(e.target.value)} />
+        <input type="number" id="weight" placeholder="Enter weight in KG" onChange={(e) => setWeight(e.target.value)} />
         
-        <input type="number" id="height" placeholder="Enter height" onChange={(event) => setHeight(event.target.value)} />
+        <input type="number" id="height" placeholder="Enter height in Cm" onChange={(event) => setHeight(event.target.value)} />
         <button type="submit" className="btn" onClick={result}>Submit</button>
         <button type="reset" className="btn">Reset</button>
         <div className="result">
-          <h3>Your BMI is :{bmii}</h3>
+          <h3>Your BMI is : {bmii}</h3>
           <p>{message}</p>
         </div>
       </form>
